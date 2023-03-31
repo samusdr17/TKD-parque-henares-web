3fe15bf932baecaa0d66b447f5cad01cbe310024
Samuel Sánchez <samusanchezdr@gmail.com>
2023-03-31 05:45:13 +0200

    servicios

---
+++ servicios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=50)
+    horarios = models.CharField(max_length=75)
+    contenido = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to = 'sevicios')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='servicio'
+        verbose_name_plural='servicios'
+
+    def __str__(self):
+        return self.titulo
